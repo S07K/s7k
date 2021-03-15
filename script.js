50f5d72527ec47690208c994721418bebc8d2ff0
S07K <shubham3431kumar@gmail.com>
2021-03-15 18:41:32 +0530

    update

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ for(var i = 0 ; i< cards.length ; i++)
 
 var infop = document.querySelectorAll("#info p");
 
-infop[3].style.margin = "5px";
-
 var heading = document.querySelector("#heading span");
 
 if(window.innerWidth >= 1000 )
@@ -18,10 +16,9 @@ if(window.innerWidth >= 1000 )
     infop[2].style.margin = "5px";
     infop[0].style.fontSize = "70px";
     infop[1].style.fontSize = "40px";
-    infop[2].style.fontSize = "38px";
-    infop[3].style.fontSize = "28px";
-    infop[2].querySelector("img").style.width = "45px";
-    infop[2].querySelector("img").style.height = "45px";
+    infop[2].style.fontSize = "30px";
+    infop[2].querySelector("img").style.width = "40px";
+    infop[2].querySelector("img").style.height = "40px";
     heading.innerHTML = "HERE ARE SOME OF MY PROJECTS";
     
     window.onscroll = function() {myFunction()};
